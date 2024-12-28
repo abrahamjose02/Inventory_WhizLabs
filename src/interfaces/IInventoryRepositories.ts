970132f0a284faf1e3ce17ceac7fdf4ed0d414ad
Abraham Jose <abrahamjose002@gmail.com>
2024-12-28 10:59:26 +0530

    feat(interfaces) : Added Interfaces explicitly for Repositories and Services

---
+++ src/interfaces/IInventoryRepositories.ts
@@ -0,0 +1,12 @@
+import { IInventoryItem } from "../types/inventoryTypes";
+
+export interface IInventoryRepository {
+  create(item: IInventoryItem): Promise<IInventoryItem>;
+  findAll(): Promise<IInventoryItem[]>;
+  findById(id: string): Promise<IInventoryItem | null>;
+  update(
+    id: string,
+    item: Partial<IInventoryItem>
+  ): Promise<IInventoryItem | null>;
+  delete(id: string): Promise<IInventoryItem | null>;
+}
---
+++ src/interfaces/IInventoryServices.ts
@@ -0,0 +1,12 @@
+import { IInventoryItem } from "../types/inventoryTypes";
+
+export interface IInventoryService {
+  createItem(item: IInventoryItem): Promise<IInventoryItem>;
+  getAllItems(): Promise<IInventoryItem[]>;
+  getItemById(id: string): Promise<IInventoryItem>;
+  updateItem(
+    id: string,
+    item: Partial<IInventoryItem>
+  ): Promise<IInventoryItem>;
+  deleteItem(id: string): Promise<void>;
+}
